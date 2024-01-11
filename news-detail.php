a694838b3864473148cd04fbc29b1af0b32ca5e2
DangKhoa96 <khoanguyenptit96@gmail.com>
2024-01-12 01:42:52 +0700

    create news detail

---
+++ news-detail.php
@@ -0,0 +1,200 @@
+<?php include "templates/head.php"; ?>
+
+<body>
+  <?php include "templates/header.php"; ?>
+  <div class="news-page__wrapper  news-detail-page__wrapper">
+    <div class="container">
+      <!-- CATEGORY LIST START -->
+      <div class="common-list-category">
+        <a href="#" target="">
+          <div class="category-item">
+            <span>Home</span>
+          </div>
+        </a>
+        <div class="arrow">
+          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4.50002 9C4.50002 9 7.49999 6.79053 7.5 5.99998C7.50001 5.20942 4.5 3 4.5 3" stroke="#6B7280"
+              stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </div>
+        <a href="#" target="">
+          <div class="category-item">
+            <span>Articles</span>
+          </div>
+        </a>
+        <div class="arrow">
+          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4.50002 9C4.50002 9 7.49999 6.79053 7.5 5.99998C7.50001 5.20942 4.5 3 4.5 3" stroke="#6B7280"
+              stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </div>
+        <a href="#" target="">
+          <div class="category-item">
+            <span>Throne of Three Kingdoms Free Redeem Codes in December 2023</span>
+          </div>
+        </a>
+      </div>
+      <!-- CATEGORY LIST END -->
+
+
+      <!-- MAIN CONTENT START -->
+      <div class="common-layout__wrapper">
+        <!-- LEFT SIDEBAR START  -->
+        <div class="common-layout__left">
+          <div class="news-detail-content__wrapper">
+            <h2 class="title">Throne of Three Kingdoms Free Redeem Codes in December 2023</h2>
+            <p class="paragraph">Unlock rewards from the latest Throne of Three Kingdoms redeem codes. Learn how to
+              redeem free codes in Throne of Three Kingdoms in this guide.</p>
+            <img src="./images/news-detail-1.png" class="image" alt="">
+            <p class="paragraph">
+              <a href="#" class="link">Throne of Three Kingdoms</a> is a new turn-based strategy game developed by
+              Gacraze
+              Entertainment Limited. Players begin the game with three kingdoms and have the opportunity to build
+              powerful armies led by legendary heroes from these factions. As it was recently released, players will
+              receive free redeem codes that can be used to obtain valuable in-game rewards and defeat their enemies. To
+              make sure you don't miss out on any of these incredible rewards, check out this comprehensive guide for
+              Throne of Three Kingdoms redeem codes.
+            </p>
+            <h3 class="sub-title">How to redeem free codes in Throne of Three Kingdoms</h3>
+            <p class="paragraph">
+              That is clear that the game has a redemption center where players can redeem their rewards by entering
+              newly
+              released or added codes. To claim your rewards, follow these simple steps and stay updated with the latest
+              codes.
+            <ol class="order-list">
+              <li> Download <a href="#" class="link">Thrones of Three Kingdoms</a> on mobile.</li>
+              <li> Begin your adventure in Throne of Three Kingdoms on any device and finish the initial quests, which
+                serve
+                as a tutorial for beginners.</li>
+              <li> Select Welfare to open the section.</li>
+              <li> Tap on the Redeem Center option to open the redemption center of this game.</li>
+              <li> Type in an active redeem code, and click on Redeem to claim your rewards from the Mail.</li>
+              <img src="./images/news-detail-2.png" class="image" alt="">
+              <h3 class="sub-title">Throne of Three Kingdoms free redeem codes and rewards</h3>
+              <p class="paragraph">
+                Fortunately, the officials have developed a valid redeem code for players to access the rewards that are
+                available. Consequently, it is important for players to adhere to these codes in order to receive
+                valuable gifts.
+              <ul>
+                <li> 100LORDSCELEBRATION -100 Ingots</li>
+                <li>TOTK2023 - SR Hero-Lady Zhen x 1, 200 x Ingot, 10 x Action Point, 5 x March Point, 1h Speed-up x 5,
+                  Lightning Thunder Frame x 1</li>
+              </ul>
+
+              Throne of Three Kingdoms offers exciting and strategic gameplay where players can take control, exert
+              power, and establish a strong reign. The creators of the game, Gacraze Entertainment Limited, aim to
+              make their product accessible to a wide audience. To achieve this goal, they provide free redeem codes
+              that come with valuable rewards for the players.
+              </p>
+            </ol>
+
+
+
+
+
+            </p>
+          </div>
+          <div class="common-section-layout__wrapper mt-24 mb-24">
+            <div class="common-section-layout__header">
+              <h2 class="common-section-layout__title">Top News</h2>
+            </div>
+          </div>
+        </div>
+        <!-- LEFT SIDEBAR START  -->
+
+        <!-- RIGHT SIDEBAR START -->
+        <div class="common-layout__right">
+          <div class="common-right-sidebar-block__wrapper">
+            <h3 class="common-right-sidebar-block__title">Latest new</h3>
+            <div class="common-right-sidebar-block__content">
+              <div class="list-news__wrapper">
+                <a href="#" target="" title="" class="common-news-item small-news">
+                  <div class="common-news-item__image-wrapper">
+                    <img src="./images/news-1.png" class="common-news-item__image" alt="">
+                  </div>
+                  <div class="common-news-item__info-wrapper">
+                    <h3 class="common-news-item__title">Reverse: 1999 Version 1.2 top New Character </h3>
+                  </div>
+                </a>
+                <a href="#" target="" title="" class="common-news-item small-news">
+                  <div class="common-news-item__image-wrapper">
+                    <img src="./images/news-2.png" class="common-news-item__image" alt="">
+                  </div>
+                  <div class="common-news-item__info-wrapper">
+                    <h3 class="common-news-item__title">Reverse: 1999 Version 1.2 top New Character Reverse: 1999
+                      Version 1.2 top New Character </h3>
+                  </div>
+                </a>
+                <a href="#" target="" title="" class="common-news-item small-news">
+                  <div class="common-news-item__image-wrapper">
+                    <img src="./images/news-3.png" class="common-news-item__image" alt="">
+                  </div>
+                  <div class="common-news-item__info-wrapper">
+                    <h3 class="common-news-item__title">Reverse: 1999 Version 1.2 top New Character Reverse: 1999
+                      Version 1.2 top New Character Reverse: 1999 Version 1.2 top New Character Reverse: 1999 Version
+                      1.2 top New Character </h3>
+                  </div>
+                </a>
+                <a href="#" target="" title="" class="common-news-item small-news">
+                  <div class="common-news-item__image-wrapper">
+                    <img src="./images/news-1.png" class="common-news-item__image" alt="">
+                  </div>
+                  <div class="common-news-item__info-wrapper">
+                    <h3 class="common-news-item__title">Reverse: 1999 Version 1.2 top New Character </h3>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- RIGHT SIDEBAR END-->
+      </div>
+      <!-- MAIN CONTENT END-->
+    </div>
+  </div>
+  <?php include "templates/footer.php"; ?>
+  <script>
+    $(function () {
+      // PC
+      var sliderBanner = $(
+        ".slider-banner-block__wrapper .slider-banner-block__list"
+      ).slick({
+        speed: 600,
+        autoplaySpeed: 6000,
+        autoplay: false,
+        infinite: true,
+        swipe: true,
+        fade: false,
+        dots: false,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        variableWidth: true,
+        responsive: [{
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            autoplay: true,
+            dots: true,
+          },
+        },
+        ],
+      });
+
+      $(".slider-banner-block__wrapper .btn-next").on("click", function () {
+        sliderBanner.slick("next");
+      });
+    });
+  </script>
+
+</body>
+
+
+</html>
